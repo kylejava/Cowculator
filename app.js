41f7e8c4601acc9025085678dacf70ab180388dd
Kyle <kyleedwardgjava@gmail.com>
2021-02-06 09:41:55 -0800

    Fixed Routing

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ app.get('/', (req, res) => {
 app.get('/subjects', (req, res) => {
   res.sendFile(path.join(__dirname + '/views/subjects.html'));
 })
+
+app.get('/grade', (req, res) => {
+  res.sendFile(path.join(__dirname + '/views/grade.html'));
+})
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
